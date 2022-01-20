962d80b312a488a91220f70bf2732b59c92cda8e
Polifron <82118872+Polifron@users.noreply.github.com>
2022-01-20 16:25:39 +0200

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Card, CardContent, CardHeader } from 'semantic-ui-react';
+
+const ReadMore = ({ children }) => {
+    const text = children;
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+        setIsReadMore(s=>!s);
+    };
+    return (
+        <p className="text">
+            {isReadMore ? text.slice(0, 50) : text}
+            <span onClick={toggleReadMore} className="read-or-hide">
+                {isReadMore ? "...read more" : " show less"}
+            </span>
+        </p>
+    );
+};
+
+const ShowMore = ({children}) => {
+    const [showMore, setShowMore] = useState(true);
+    const tasks = children;
+    const [unit, setUnit] = useState("Down");
+    const toggleShowMore = () => {
+        setShowMore(s => !s);
+        showMore ? setUnit("Up") : setUnit("Down")
+
+    }
+    return (
+
+        <div>
+
+        <p  className="taks">
+            {showMore ? "" : tasks }
+
+        </p>
+            <button className="showMore" onClick={toggleShowMore}> {unit}</button>
+        </div>
+    );
+};
+
+const logo = require('./Avatar-PNG-Pic.png');
+
+
+const Activity = () => {
+    
+    return (
+        
+        <Card>
+            <CardHeader>
+                <img className="avatar" src={logo} />
+                <div className="rightHeader">
+                    <div className="user__name">
+                    Polifron ionut</div>
+                    <p className="dateAdded"> At: 10-12-21 </p>
+                </div>
+                <div className="settingsDropDown">
+                    ...
+                </div>
+                <div className="activityTitle">
+                    Do your job motherfuker!
+                </div>
+            </CardHeader>
+            <CardContent>
+                <div className="description">
+                    <ReadMore>
+                        GeeksforGeeks: A Computer Science portal for geeks.
+                        It contains well written, well thought and well explained
+                        computer science, programming articles and quizzes.
+                        It provides a variety of services for you to learn, so thrive
+                        and also have fun! Free Tutorials, Millions of Articles, Live,
+                        Online and Classroom Courses ,Frequent Coding Competitions,
+                        Webinars by Industry Experts, Internship opportunities, and Job
+                        Opportunities. Knowledge is power!
+                    </ReadMore>
+                </div>
+
+
+                <div className="task_footer">
+                    <p>Tasks <span>3/12</span></p>
+                    <p>Due to: 10-02-22</p>
+                </div>
+
+                <ShowMore>
+                    <div className="taskWarper">
+                        <div>Task</div>
+                        <input type="checkbox" />
+                    </div>
+                    <div className="taskWarper">
+                        <div>Task</div>
+                        <input type="checkbox" />
+                    </div>
+                    <div className="taskWarper">
+                        <div>Task</div>
+                        <input type="checkbox" />
+                    </div>
+
+
+
+                </ShowMore>
+            </CardContent>
+            
+        </Card>
+    );
+}
+
+export default Activity;
